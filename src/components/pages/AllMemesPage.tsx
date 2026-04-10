176abc02d66ae9055428a8635091884a723b7b5f
samofwise <sam.ed.henry@gmail.com>
2026-04-10 11:45:35 +1000

    add is a meme check

--- src/components/pages/AllMemesPage.tsx
+++ src/components/pages/AllMemesPage.tsx
@@ -1,47 +1,72 @@
 import { getAllMessages } from "~/api/memes";
 import { MemeItem } from "~/components/atoms/MemeItem";
 
+/** Gridlines + padding once on the table via descendant selectors */
+const tableClassName =
+  "w-full min-w-[640px] table-fixed border-collapse border border-white/15 " +
+  "[&_th]:border [&_th]:border-white/15 [&_th]:px-3 [&_th]:py-2 [&_th]:text-left [&_th]:align-top " +
+  "[&_thead_th]:sticky [&_thead_th]:top-0 [&_thead_th]:z-10 [&_thead_th]:bg-slate-950/95 [&_thead_th]:backdrop-blur-sm supports-backdrop-filter:[&_thead_th]:bg-slate-950/80 " +
+  "[&_td]:border [&_td]:border-white/15 [&_td]:px-3 [&_td]:py-2 [&_td]:align-top " +
+  "[&_tbody_tr]:even:bg-white/4";
+
 export const AllMessagesPage = () => {
   const messages = getAllMessages();
   return (
     <section className="flex w-full flex-col items-center p-4">
-      <article className="flex flex-col justify-center">
-        <table className="w-full table-fixed border-collapse border border-white/15">
-          <thead>
-            <tr>
-              <th className="border border-white/15 px-3 py-2 text-left">
-                Posted At
-              </th>
-              <th className="border border-white/15 px-3 py-2 text-left">
-                Author
-              </th>
-              <th className="w-[150px] border border-white/15 px-3 py-2 text-left">
-                Meme
-              </th>
-              <th className="border border-white/15 px-3 py-2 text-left">
-                Data
-              </th>
-            </tr>
-          </thead>
-          <tbody>
-            {messages.map((message, i) => (
-              <tr key={i}>
-                <td className="border border-white/15 px-3 py-2 align-top">
-                  {message.postedAt.toLocaleString()}
-                </td>
-                <td className="border border-white/15 px-3 py-2 align-top">
-                  {message.postedBy}
-                </td>
-                <td className="border border-white/15 px-3 py-2 align-top">
-                  {message.meme ? <MemeItem meme={message.meme} /> : null}
-                </td>
-                <td className="border border-white/15 px-3 py-2 align-top">
-                  {message.data}
-                </td>
+      <article className="flex w-full max-w-full flex-col justify-center">
+        <div className="overflow-x-auto rounded-lg">
+          <table className={tableClassName}>
+            <colgroup>
+              <col />
+              <col />
+              <col className="w-[150px]" />
+              <col />
+            </colgroup>
+            <thead>
+              <tr>
+                <th scope="col">Posted At</th>
+                <th scope="col">Author</th>
+                <th scope="col">Meme</th>
+                <th scope="col">Data</th>
+                <th scope="col">Is A Meme</th>
               </tr>
-            ))}
-          </tbody>
-        </table>
+            </thead>
+            <tbody>
+              {messages.map((message, i) => (
+                <tr key={`${message.postedAt.toISOString()}-${i}`}>
+                  <td>{message.postedAt.toLocaleString()}</td>
+                  <td>{message.postedBy}</td>
+                  <td>
+                    {message.meme ? <MemeItem meme={message.meme} /> : null}
+                  </td>
+                  <td>{message.data}</td>
+                  <td>
+                    <div>
+                      <label>
+                        <input
+                          type="radio"
+                          name={`is-meme-${i}`}
+                          value="Yes"
+                          checked={!!message.meme}
+                        />
+                        Yes
+                      </label>
+                      <label className="ml-2">
+                        <input
+                          type="radio"
+                          name={`is-meme-${i}`}
+                          value="No"
+                          checked={!message.meme}
+                        />
+                        No
+                      </label>
+                    </div>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       </article>
     </section>
   );
